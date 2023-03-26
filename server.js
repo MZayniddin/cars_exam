3825f0fdb2a16f4de443b14bc9df13e48b15b8be
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-03-26 12:53:48 +0500

    added 404 html

--- server.js
+++ server.js
@@ -30,7 +30,9 @@ app.use(express.static(path.join(__dirname, "public")));
 // 404 NOT FOUND
 app.all("*", (req, res) => {
     res.status(404);
-    if (req.accepts("json")) {
+    if (req.accepts("html")) {
+        res.sendFile(path.join(__dirname, "views", "404.html"));
+    } else if (req.accepts("json")) {
         res.send({ message: "404 Not Found" });
     } else {
         res.type("txt").send("404 Not Found");
