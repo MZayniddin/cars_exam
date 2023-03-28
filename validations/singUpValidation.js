0c6264d1297fdf1dcbdfb3353344faaf7b119a3a
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-03-28 12:46:21 +0500

    added sign up validation

---
+++ validations/singUpValidation.js
@@ -0,0 +1,22 @@
+const Joi = require("joi");
+
+const signUpValidation = async (req, res, next) => {
+    const schema = Joi.object({
+        email: Joi.string()
+            .email({
+                minDomainSegments: 2,
+                tlds: { allow: ["com", "net", "ru"] },
+            })
+            .required(),
+        password: Joi.string()
+            .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+            .required(),
+    });
+
+    try {
+        const result = await schema.validateAsync(req.body);
+        if (result) next();
+    } catch (err) {
+        next({ message: err.details[0].message, status: 406 });
+    }
+};
