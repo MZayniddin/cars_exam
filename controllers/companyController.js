9a7c1779ddb3be594d1548a435e184083fdfc177
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-03-28 16:30:05 +0500

    added create new company api

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -1,7 +1,35 @@
 const pool = require("../config/dbCon");
 
-const createNewCompany = async (req, res) => {
-    
+const createNewCompany = async (req, res, next) => {
+    const { name, email, address } = req.body;
+    try {
+        // CHECK EMAIL TO DUPLICATE
+        const checkEmail = await pool.query(
+            "SELECT name FROM emails WHERE name=$1",
+            [email]
+        );
+
+        if (checkEmail.rows[0])
+            return res.status(409).json({
+                message: "This email is already in use by another account",
+            });
+
+        // STORE EMAIL TO DB AND GET ID
+        await pool.query("INSERT INTO emails(name) VALUES($1)", [email]);
+        const newEmailId = await (
+            await pool.query("SELECT id FROM emails WHERE name=$1", [email])
+        ).rows[0].id;
+
+        // STORE NEW COMPANY
+        await pool.query(
+            "INSERT INTO Company(name, email_id, address, created_by) VALUES($1, $2, $3, $4)",
+            [name, newEmailId, address, req.user]
+        );
+
+        res.status(201).json({ message: `New ${name} company created!` });
+    } catch (err) {
+        next(err);
+    }
 };
 
 module.exports = { createNewCompany };
