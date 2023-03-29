2d85f1b32d7f685bc69139eec42e6f47f6bffe44
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-03-29 11:40:46 +0500

    fixed create company

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -3,16 +3,28 @@ const pool = require("../config/dbCon");
 const createNewCompany = async (req, res, next) => {
     const { name, email, address } = req.body;
     try {
-        // CHECK EMAIL TO DUPLICATE
+        // CHECK TO DUPLICATE
         const checkEmail = await pool.query(
             "SELECT name FROM emails WHERE name=$1",
             [email]
+        );
+
+        const checkName = await pool.query(
+            "SELECT name FROM company WHERE name=$1",
+            [name]
         );
 
         if (checkEmail.rows[0])
             return res.status(409).json({
                 message: "This email is already in use by another account",
             });
+        else if (checkName.rows[0]) {
+            return res
+                .status(409)
+                .json({
+                    message: `This name is already in use by another account`,
+                });
+        }
 
         // STORE EMAIL TO DB AND GET ID
         await pool.query("INSERT INTO emails(name) VALUES($1)", [email]);
