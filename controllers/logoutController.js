2bd1ea3688dafe526af9a517394fd1a99d92f66d
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-03-31 16:36:23 +0500

    added logout api

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -1,7 +1,22 @@
 const pool = require("../config/dbCon");
 
 const handleLogout = async (req, res) => {
-    res.send("OK");
+    if (!req.cookies?.jwt) return res.sendStatus(204);
+    const refreshToken = req.cookies.jwt;
+
+    // CLEAR USER'S REFRESH TOKEN
+    await pool.query(
+        "UPDATE Users SET refresh_token='' WHERE refresh_token=$1",
+        [refreshToken]
+    );
+
+    res.clearCookie("jwt", {
+        httpOnly: true,
+        sameSite: "None",
+        maxAge: 24 * 60 * 60 * 100,
+    });
+
+    res.sendStatus(204);
 };
 
 module.exports = { handleLogout };
