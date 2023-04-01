c534109bc37179ccfa9148a1c2e8658ef2d8c7f2
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-04-01 07:05:33 +0500

    added create new car api

--- controllers/carsController.js
+++ controllers/carsController.js
@@ -9,13 +9,17 @@ const createNewCar = async (req, res) => {
         });
 
     // CHECK USER HAS COMPANY
-
     if (!req.companyId)
         return res
             .status(400)
             .json({ message: "First, you should create a company!" });
-    
-    res.send("OK")
+
+    await pool.query(
+        "INSERT INTO Cars(name, price, color, brand, created_by, company_id) VALUES($1, $2, $3, $4, $5, $6)",
+        [name, price, color, brand, req.user, req.companyId]
+    );
+
+    res.status(201).json({ message: `New car ${name} created!` });
 };
 
 module.exports = { createNewCar };
